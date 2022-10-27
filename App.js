b62dca0ffa07a9f4b8ed8c187458d5f81d4a6aa0
DavidCacha <31908047+DavidCacha@users.noreply.github.com>
2022-10-26 20:09:08 -0500

    Cambios en screen

--- App.js
+++ App.js
@@ -60,32 +60,11 @@ const App: () => Node = () => {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={{backgroundColor:"black", flex:1}}>
+        <View style={{height:"100%" ,justifyContent:"center", alignItems:"center"}}>
+          <Text style={{color:"white", fontSize:58}}>Hola mundo</Text>
         </View>
-      </ScrollView>
+      
     </SafeAreaView>
   );
 };
